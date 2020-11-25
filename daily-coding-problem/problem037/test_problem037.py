83e95d13a32e862a43a527e5a4d6ec97e1ab06d5
Sean Moore <sean.m.moore@gmail.com>
2020-11-24 22:45:31 -0800

    Implement daily coding problem 37.

---
+++ daily-coding-problem/problem037/test_problem037.py
@@ -0,0 +1,12 @@
+__author__ = 'Sean Moore'
+
+import pytest
+from problem037 import *
+
+@pytest.mark.parametrize('input,expected', [
+    ({1,2,3}, set(frozenset(x) for x in
+        [[],[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]))
+])
+def test_power_set(input, expected):
+    assert power_set(input) == expected
+    assert power_set_recursive(input) == expected
