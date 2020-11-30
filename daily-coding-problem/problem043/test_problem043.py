be7aa8346634fa3b287e3f745f4ca0b6237c3e71
Sean Moore <sean.m.moore@gmail.com>
2020-11-30 15:50:33 -0800

    Implement daily coding problem 43.

---
+++ daily-coding-problem/problem043/test_problem043.py
@@ -0,0 +1,20 @@
+__author__ = 'Sean Moore'
+
+import pytest
+from problem043 import Stack
+
+@pytest.mark.parametrize('input', [
+    (('push',1),('push',2),('max',2),('pop',2),('max',1),('pop',1),
+     ('pop',None))
+])
+def test_stack(input):
+    stack = Stack()
+    for op, val in input:
+        if op == 'push':
+            stack.push(val)
+        elif op == 'pop':
+            top = stack.pop()
+            assert top == val
+        elif op == 'max':
+            max_val = stack.max()
+            assert max_val == val
